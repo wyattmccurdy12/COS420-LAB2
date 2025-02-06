aff2b092d65b4b7c244a7169ced3d4a2c5a25aae
Wyatt McCurdy <36129149+wyattmccurdy12@users.noreply.github.com>
2025-02-06 14:10:00 -0500

    first commit

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hear ye Hear ye! Important announcement!");
+        System.out.println("Hello, World!");
+    }
+}
